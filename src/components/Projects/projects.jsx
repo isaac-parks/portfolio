77f917df38089e2d470553f7ca8d746a29be4f71
Isaac Parks <isaacparks1800@gmail.com>
2021-11-29 18:27:26 -0600

    started making website changes - fixed typography

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -17,7 +17,7 @@ export default function Projects() {
     <div className="projects transition">
       <div className="footer-header">
         <h1>
-          <span style={{ fontSize: "5rem" }}>
+          <span style={{ fontSize: "4rem" }}>
             {" "}
             I <span style={{ color: "red" }}>❤️ </span> Creating -{" "}
           </span>{" "}

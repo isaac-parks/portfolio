1006f764418afe4565f0c320feb21a3529b2a2c4
Isaac Parks <isaacparks1800@gmail.com>
2026-01-02 01:21:31 -0600

    bump year

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="copyright">
-        <p>© 2023 Isaac Parks. All Rights Reserved.</p>
+        <p>© 2026 Isaac Parks. All Rights Reserved.</p>
       </div>
     </footer>
   );
